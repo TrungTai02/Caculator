e1c0a4a9e73719284f665ef5dbec80f8fa9d626d
TrungTai02 <2051010269tai@ou.edu.vn>
2023-11-03 11:05:38 +0700

    xu li dieu kien cong

--- Caculator/Form1.cs
+++ Caculator/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Caculator
@@ -20,20 +13,27 @@ namespace Caculator
 
         private void button2_Click(object sender, EventArgs e)
         {
-           
+         
                 double a = double.Parse(txtA.Text);
                 double b = double.Parse(txtB.Text);
                 double c = a - b;
                 txtKetQua.Text = c.ToString();
+
+            
+            
             
         }
 
         private void btn_cong_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(txtA.Text);
-            double b = double.Parse(txtB.Text);
-            double c = a + b;
-            txtKetQua.Text = c.ToString();
+            if(txtA.Text!=String.Empty)
+            {
+                double a = double.Parse(txtA.Text);
+                double b = double.Parse(txtB.Text);
+                double c = a + b;
+                txtKetQua.Text = c.ToString();
+            }    
+          
         }
     }
 }
